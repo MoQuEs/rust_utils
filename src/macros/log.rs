2ca396839bd1f604020f702b85be134e93b2653f
Sebastian Kaźmierczak <moques93@gmail.com>
2024-09-24 03:57:01 +0200

    simplify main log macro

--- src/macros/log.rs
+++ src/macros/log.rs
@@ -1,60 +1,60 @@
 #[macro_export]
 macro_rules! res_log {
-    ($code:expr; $e:ident; target: $target:expr, $lvl:expr, $($arg:tt),+) => {
+    ($code:expr; $e:ident; target: $target:expr, $lvl:ident, $($arg:tt),+) => {
         match $code {
             Ok(val) => Ok(val),
             Err($e) => {
-                log::log!(target: $target, $lvl, $($arg),+);
+                log::log!(target: $target, log::Level::$lvl, $($arg),+);
                 Err($e)
             }
         }
     };
 
-    ($code:expr; target: $target:expr, $lvl:expr, $($arg:tt),+) => {
+    ($code:expr; target: $target:expr, $lvl:ident, $($arg:tt),+) => {
         match $code {
             Ok(val) => Ok(val),
             Err(err) => {
-                log::log!(target: $target, $lvl, $($arg),+);
+                log::log!(target: $target, log::Level::$lvl, $($arg),+);
                 Err(err)
             }
         }
     };
 
-    ($code:expr; target: $target:expr, $lvl:expr) => {
+    ($code:expr; target: $target:expr, $lvl:ident) => {
         match $code {
             Ok(val) => Ok(val),
             Err(err) => {
-                log::log!(target: $target, $lvl, "Error: {err:?}");
+                log::log!(target: $target, log::Level::$lvl, "Error: {err:?}");
                 Err(err)
             }
         }
     };
 
-    ($code:expr; $e:ident; $lvl:expr, $($arg:tt),+) => {
+    ($code:expr; $e:ident; $lvl:ident, $($arg:tt),+) => {
         match $code {
             Ok(val) => Ok(val),
             Err($e) => {
-                log::log!($lvl, $($arg),+);
+                log::log!(log::Level::$lvl, $($arg),+);
                 Err($e)
             }
         }
     };
 
-    ($code:expr; $lvl:expr, $($arg:tt),+) => {
+    ($code:expr; $lvl:ident, $($arg:tt),+) => {
         match $code {
             Ok(val) => Ok(val),
             Err(err) => {
-                log::log!($lvl, $($arg),+);
+                log::log!(log::Level::$lvl, $($arg),+);
                 Err(err)
             }
         }
     };
 
-    ($code:expr; $lvl:expr) => {
+    ($code:expr; $lvl:ident) => {
         match $code {
             Ok(val) => Ok(val),
             Err(err) => {
-                log::log!($lvl, "Error: {err:?}");
+                log::log!(log::Level::$lvl, "Error: {err:?}");
                 Err(err)
             }
         }
@@ -63,21 +63,21 @@ macro_rules! res_log {
 
 #[macro_export]
 macro_rules! opt_log {
-    ($code:expr; target: $target:expr, $lvl:expr, $($arg:tt),+) => {
+    ($code:expr; target: $target:expr, $lvl:ident, $($arg:tt),+) => {
         match $code {
             Some(val) => Some(val),
             None => {
-                log::log!(target: $target, $lvl, $($arg),+);
+                log::log!(target: $target, log::Level::$lvl, $($arg),+);
                 None
             }
         }
     };
 
-    ($code:expr; $lvl:expr, $($arg:tt),+) => {
+    ($code:expr; $lvl:ident, $($arg:tt),+) => {
         match $code {
             Some(val) => Some(val),
             None => {
-                log::log!($lvl, $($arg),+);
+                log::log!(log::Level::$lvl, $($arg),+);
                 None
             }
         }
@@ -87,190 +87,190 @@ macro_rules! opt_log {
 #[macro_export]
 macro_rules! res_error {
     ($code:expr; $e:ident; target: $target:expr, $($arg:tt),+) => {
-        res_log!($code; $e; target: $target, log::Level::Error, $($arg),+)
+        res_log!($code; $e; target: $target, Error, $($arg),+)
     };
 
     ($code:expr; target: $target:expr, $($arg:tt),+) => {
-        res_log!($code; target: $target, log::Level::Error, $($arg),+)
+        res_log!($code; target: $target, Error, $($arg),+)
     };
 
     ($code:expr; target: $target:expr) => {
-        res_log!($code; target: $target, log::Level::Error)
+        res_log!($code; target: $target, Error)
     };
 
     ($code:expr; $e:ident; $($arg:tt),+) => {
-        res_log!($code; $e; log::Level::Error, $($arg),+)
+        res_log!($code; $e; Error, $($arg),+)
     };
 
     ($code:expr; $($arg:tt),+) => {
-        res_log!($code; log::Level::Error, $($arg),+)
+        res_log!($code; Error, $($arg),+)
     };
 
     ($code:expr;) => {
-        res_log!($code; log::Level::Error)
+        res_log!($code; Error)
     };
 }
 
 #[macro_export]
 macro_rules! opt_error {
     ($code:expr; target: $target:expr, $($arg:tt),+) => {
-        opt_log!($code; target: $target, log::Level::Error, $($arg),+)
+        opt_log!($code; target: $target, Error, $($arg),+)
     };
 
     ($code:expr; $($arg:tt),+) => {
-        opt_log!($code; log::Level::Error, $($arg),+)
+        opt_log!($code; Error, $($arg),+)
     };
 }
 
 #[macro_export]
 macro_rules! res_warn {
     ($code:expr; $e:ident; target: $target:expr, $($arg:tt),+) => {
-        res_log!($code; $e; target: $target, log::Level::Warn, $($arg),+)
+        res_log!($code; $e; target: $target, Warn, $($arg),+)
     };
 
     ($code:expr; target: $target:expr, $($arg:tt),+) => {
-        res_log!($code; target: $target, log::Level::Warn, $($arg),+)
+        res_log!($code; target: $target, Warn, $($arg),+)
     };
 
     ($code:expr; target: $target:expr) => {
-        res_log!($code; target: $target, log::Level::Warn)
+        res_log!($code; target: $target, Warn)
     };
 
     ($code:expr; $e:ident; $($arg:tt),+) => {
-        res_log!($code; $e; log::Level::Warn, $($arg),+)
+        res_log!($code; $e; Warn, $($arg),+)
     };
 
     ($code:expr; $($arg:tt),+) => {
-        res_log!($code; log::Level::Warn, $($arg),+)
+        res_log!($code; Warn, $($arg),+)
     };
 
     ($code:expr;) => {
-        res_log!($code; log::Level::Warn)
+        res_log!($code; Warn)
     };
 }
 
 #[macro_export]
 macro_rules! opt_warn {
     ($code:expr; target: $target:expr, $($arg:tt),+) => {
-        opt_log!($code; target: $target, log::Level::Warn, $($arg),+)
+        opt_log!($code; target: $target, Warn, $($arg),+)
     };
 
     ($code:expr; $($arg:tt),+) => {
-        opt_log!($code; log::Level::Warn, $($arg),+)
+        opt_log!($code; Warn, $($arg),+)
     };
 }
 
 #[macro_export]
 macro_rules! res_info {
     ($code:expr; $e:ident; target: $target:expr, $($arg:tt),+) => {
-        res_log!($code; $e; target: $target, log::Level::Info, $($arg),+)
+        res_log!($code; $e; target: $target, Info, $($arg),+)
     };
 
     ($code:expr; target: $target:expr, $($arg:tt),+) => {
-        res_log!($code; target: $target, log::Level::Info, $($arg),+)
+        res_log!($code; target: $target, Info, $($arg),+)
     };
 
     ($code:expr; target: $target:expr) => {
-        res_log!($code; target: $target, log::Level::Info)
+        res_log!($code; target: $target, Info)
     };
 
     ($code:expr; $e:ident; $($arg:tt),+) => {
-        res_log!($code; $e; log::Level::Info, $($arg),+)
+        res_log!($code; $e; Info, $($arg),+)
     };
 
     ($code:expr; $($arg:tt),+) => {
-        res_log!($code; log::Level::Info, $($arg),+)
+        res_log!($code; Info, $($arg),+)
     };
 
     ($code:expr;) => {
-        res_log!($code; log::Level::Info)
+        res_log!($code; Info)
     };
 }
 
 #[macro_export]
 macro_rules! opt_info {
     ($code:expr; target: $target:expr, $($arg:tt),+) => {
-        opt_log!($code; target: $target, log::Level::Info, $($arg),+)
+        opt_log!($code; target: $target, Info, $($arg),+)
     };
 
     ($code:expr; $($arg:tt),+) => {
-        opt_log!($code; log::Level::Info, $($arg),+)
+        opt_log!($code; Info, $($arg),+)
     };
 }
 
 #[macro_export]
 macro_rules! res_debug {
     ($code:expr; $e:ident; target: $target:expr, $($arg:tt),+) => {
-        res_log!($code; $e; target: $target, log::Level::Debug, $($arg),+)
+        res_log!($code; $e; target: $target, Debug, $($arg),+)
     };
 
     ($code:expr; target: $target:expr, $($arg:tt),+) => {
-        res_log!($code; target: $target, log::Level::Debug, $($arg),+)
+        res_log!($code; target: $target, Debug, $($arg),+)
     };
 
     ($code:expr; target: $target:expr) => {
-        res_log!($code; target: $target, log::Level::Debug)
+        res_log!($code; target: $target, Debug)
     };
 
     ($code:expr; $e:ident; $($arg:tt),+) => {
-        res_log!($code; $e; log::Level::Debug, $($arg),+)
+        res_log!($code; $e; Debug, $($arg),+)
     };
 
     ($code:expr; $($arg:tt),+) => {
-        res_log!($code; log::Level::Debug, $($arg),+)
+        res_log!($code; Debug, $($arg),+)
     };
 
     ($code:expr;) => {
-        res_log!($code; log::Level::Debug)
+        res_log!($code; Debug)
     };
 }
 
 #[macro_export]
 macro_rules! opt_debug {
     ($code:expr; target: $target:expr, $($arg:tt),+) => {
-        opt_log!($code; target: $target, log::Level::Debug, $($arg),+)
+        opt_log!($code; target: $target, Debug, $($arg),+)
     };
 
     ($code:expr; $($arg:tt),+) => {
-        opt_log!($code; log::Level::Debug, $($arg),+)
+        opt_log!($code; Debug, $($arg),+)
     };
 }
 
 #[macro_export]
 macro_rules! res_trace {
     ($code:expr; $e:ident; target: $target:expr, $($arg:tt),+) => {
-        res_log!($code; $e; target: $target, log::Level::Trace, $($arg),+)
+        res_log!($code; $e; target: $target, Trace, $($arg),+)
     };
 
     ($code:expr; target: $target:expr, $($arg:tt),+) => {
-        res_log!($code; target: $target, log::Level::Trace, $($arg),+)
+        res_log!($code; target: $target, Trace, $($arg),+)
     };
 
     ($code:expr; target: $target:expr) => {
-        res_log!($code; target: $target, log::Level::Trace)
+        res_log!($code; target: $target, Trace)
     };
 
     ($code:expr; $e:ident; $($arg:tt),+) => {
-        res_log!($code; $e; log::Level::Trace, $($arg),+)
+        res_log!($code; $e; Trace, $($arg),+)
     };
 
     ($code:expr; $($arg:tt),+) => {
-        res_log!($code; log::Level::Trace, $($arg),+)
+        res_log!($code; Trace, $($arg),+)
     };
 
     ($code:expr;) => {
-        res_log!($code; log::Level::Trace)
+        res_log!($code; Trace)
     };
 }
 
 #[macro_export]
 macro_rules! opt_trace {
     ($code:expr; target: $target:expr, $($arg:tt),+) => {
-        opt_log!($code; target: $target, log::Level::Trace, $($arg),+)
+        opt_log!($code; target: $target, Trace, $($arg),+)
     };
 
     ($code:expr; $($arg:tt),+) => {
-        opt_log!($code; log::Level::Trace, $($arg),+)
+        opt_log!($code; Trace, $($arg),+)
     };
 }
 
@@ -341,6 +341,13 @@ mod test {
             let _ = res_error!(
                 $f("f");
             );
+
+            let _ = res_log!(
+                $f("x"); err;
+                target: "target",
+                Error,
+                "99 {err}"
+            );
         };
     }
 
@@ -355,6 +362,13 @@ mod test {
             let _ = opt_trace!(
                 $f("e");
                 "4"
+            );
+
+            let _ = opt_log!(
+                $f("x");
+                target: "target",
+                Error,
+                "99"
             );
         };
     }
@@ -377,7 +391,7 @@ mod test {
         test_maker_result!(err_s);
 
         testing_logger::validate(|captured_logs| {
-            assert_eq!(captured_logs.len(), 6);
+            assert_eq!(captured_logs.len(), 7);
 
             assert_eq!(captured_logs[0].body, "0 a");
             assert_eq!(captured_logs[0].level, log::Level::Error);
@@ -396,6 +410,9 @@ mod test {
 
             assert_eq!(captured_logs[5].body, "Error: \"f\"");
             assert_eq!(captured_logs[5].level, log::Level::Error);
+
+            assert_eq!(captured_logs[6].body, "99 x");
+            assert_eq!(captured_logs[6].level, log::Level::Error);
         });
     }
 
@@ -406,7 +423,7 @@ mod test {
         test_maker_result!(err_impl);
 
         testing_logger::validate(|captured_logs| {
-            assert_eq!(captured_logs.len(), 6);
+            assert_eq!(captured_logs.len(), 7);
 
             assert_eq!(captured_logs[0].body, "0 err a");
             assert_eq!(captured_logs[0].level, log::Level::Error);
@@ -425,6 +442,9 @@ mod test {
 
             assert_eq!(captured_logs[5].body, "Error: Error(\"f\")");
             assert_eq!(captured_logs[5].level, log::Level::Error);
+
+            assert_eq!(captured_logs[6].body, "99 err x");
+            assert_eq!(captured_logs[6].level, log::Level::Error);
         });
     }
 
@@ -446,13 +466,16 @@ mod test {
         test_maker_option!(none);
 
         testing_logger::validate(|captured_logs| {
-            assert_eq!(captured_logs.len(), 2);
+            assert_eq!(captured_logs.len(), 3);
 
             assert_eq!(captured_logs[0].body, "1");
             assert_eq!(captured_logs[0].level, log::Level::Warn);
 
             assert_eq!(captured_logs[1].body, "4");
             assert_eq!(captured_logs[1].level, log::Level::Trace);
+
+            assert_eq!(captured_logs[2].body, "99");
+            assert_eq!(captured_logs[2].level, log::Level::Error);
         });
     }
 }
